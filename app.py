23d73e488e947164f0ebe530148bfd7c7623703a
Chris Ruffalo <chris.ruffalo@gmail.com>
2022-08-22 09:06:11 -0400

    updated to support full url or partial (base) url matching added better matching logic (non matched paths are not forwarded) added logging for outbound requests reflected changes in new configuration examples

--- app.py
+++ app.py
@@ -20,17 +20,30 @@ app = Flask(__name__)
 # storage for configuration items
 params = {}
 compiled = {}
+base_urls = {}
+urls = {}
+
+
+# chop the end off of a string if that end is a slash
+def fix(url):
+    if url.endswith("/"):
+        url = url[0:-1]
+    return url
+
 
 # parse the configuration file
 with open("redirect.yml", "r") as ymlfile:
     cfg = load(ymlfile, Loader=Loader)
-    base_url = cfg['base_url']
-    if not base_url.endswith('/'):
-        base_url = base_url + "/"
     for r in cfg['redirects']:
         match = r['match']
         params[match] = r['params']
         compiled[match] = re.compile(match)
+        if 'base_url' in r and 'url' in r:
+            raise Exception("A base_url or a url must be specified, not both (match string: " + match + ")")
+        if 'base_url' in r:
+            base_urls[match] = fix(r['base_url'])
+        if 'url' in r:
+            urls[match] = fix(r['url'])
 
 
 # any request (except to the test path) is treated as a single parameter that will
@@ -49,31 +62,40 @@ def redirect(path):
         for k in request.args:
             redirected_params[k] = request.args[k]
 
-    # if a payload was provided we need to do things with it
+    # match url
+    for url_match in params:
+        c = compiled[url_match]
+        # use the compiled matcher against the decoded path and the encoded path to prevent surprises
+        if c.match(decoded_path) or c.match(path):
+            found_params = params[url_match]
+            if url_match in base_urls:
+                remote_path = base_urls[url_match] + "/" + urllib.parse.quote(decoded_path)
+                break
+            elif url_match in urls:
+                remote_path = urls[url_match]
+                break
+
+    if not remote_path:
+        print("no match found for path: '" + decoded_path + "'")
+        return "no match for path", 404
+
+    # if a payload was provided we need build parameters from the (hopefully json) payload
     if request.method == 'POST' or request.method == 'PUT':
         raw_payload = request.get_data()
         # only try and process the payload if we have one
-        if len(raw_payload) > 0:
-            # check against all the URL matches, this doesn't happen often enough to optimize
-            for url_match in params:
-                c = compiled[url_match]
-                # use the compiled matcher against the decoded path and the encoded path to prevent surprises
-                if c.match(decoded_path) or c.match(path):
-                    found_params = params[url_match]
+        if found_params and len(raw_payload) > 0:
+            # if a match is found use the jsonpath to populate query parameters that will be passed
+            # the consuming endpoint
+            for parameter_pair in found_params:
+                payload = json.loads(raw_payload)
+                param_key = parameter_pair['name']
+                param_json_path = parameter_pair['path']
+                found_value = parse(param_json_path).find(payload)
+                if found_value and found_value[0] and found_value[0].value:
+                    redirected_params[param_key] = found_value[0].value
 
-                    # if a match is found use the jsonpath to populate query parameters that will be passed
-                    # the consuming endpoint
-                    for parameter_pair in found_params:
-                        payload = json.loads(raw_payload)
-                        param_key = parameter_pair['name']
-                        param_json_path = parameter_pair['path']
-                        found_value = parse(param_json_path).find(payload)
-                        if found_value and found_value[0] and found_value[0].value:
-                            redirected_params[param_key] = found_value[0].value
-
-    # ensure the path we used is the encoded version of the earlier decoded path
-    path = urllib.parse.quote(decoded_path)
-    remote_path = base_url + path
+    # log request
+    print("forwarding:", request.method, remote_path, request.query_string, request.get_data())
 
     # build out http request, if it is a get request no need to send a payload
     if 'GET' == request.method:
